c0713529a6d73f4ac13ae93336da4f9906431174
lahir <mohamedlahir1234@gmail.com>
2021-09-27 00:13:36 +0530

    NA

--- HiStudent.com/src/main/java/com/demo/accountmodelrepo/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.demo.accountmodelrepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.studentaccount.StudentAccount;
-
-public interface AccountRepo extends JpaRepository<StudentAccount, Integer>{
-
-}
